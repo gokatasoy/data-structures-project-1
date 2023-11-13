9b58d7351a297ba4b0bdae73b5f611ee564140d7
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-11-13 15:52:57 +0300

    Add comment lines to nodes.

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,17 +16,22 @@ int main(){
     //Dosyadan okunan her sayı değeri:
     int sayiDeger;
     while (dosya >> sayiDeger){
+        //Yeni bir sayı değeri oluşturulur.
         Sayi sayi;
+
+        //SayiDeger'in her bir basamağını Sayi nesnesine eklemek için:
         while (sayiDeger > 0){
             int basamak = sayiDeger % 10;
             sayi.ekleBasamak(basamak);
             sayiDeger /= 10;
         }
+        //Oluşturulan Sayi nesnesi, SayilarListesi'ne ekleriz.
         sayilarListesi.ekleSayi(sayi);
     }
 
     dosya.close();
 
+    
     cout<<"Sayilar Listesi: "<<endl;
     sayilarListesi.yazdir();
 
